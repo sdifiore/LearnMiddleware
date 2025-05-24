99db916cf276dd5cf5b26dc1d976e6a9c691cb8d
Sergio Di Fiore <difiores@outlook.com>
2025-05-24 10:16:38 -0300

    app.Run will always create a terminal middleware

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -5,34 +5,27 @@ var app = builder.Build();
 app.Use(async (HttpContext context, RequestDelegate next) =>
 {
     await context.Response.WriteAsync("Middleware #1: Before calling next\n");
-    // First call based purely context
 
     await next(context);
 
     await context.Response.WriteAsync("Middleware #1: After calling next\n");
-    // Context most likely changed by previous instructions
 });
 
 // Middleware #2
-app.Use(async (HttpContext context, RequestDelegate next) =>
+app.Run(async (context) =>
+//app.Run will always create a terminal middleware
 {
     await context.Response.WriteAsync("Middleware #2: Before calling next\n");
-
-    //await next(context);
-    // Calling next is optional, if not called, the request will not proceed to the next middleware
-    // Thus this is a terminal midleware
-
-    await context.Response.WriteAsync("Middleware #2: After calling next\n");
 });
 
 // Middleware #3
-app.Use(async (context, next) =>
+app.Use(async (HttpContext context, RequestDelegate next) =>
 {
     await context.Response.WriteAsync("Middleware #3: Before calling next\n");
 
     await next(context);
-    //remving HttpContext and RequestDelegate make required using next(context)
 
     await context.Response.WriteAsync("Middleware #3: After calling next\n");
 });
+
 app.Run();
